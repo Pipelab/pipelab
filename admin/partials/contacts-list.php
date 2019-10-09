a0d3946a5509a2a56e9fec56a0dbf2eeee181730
Julien Liabeuf <julien@liabeuf.fr>
2019-10-09 22:21:53 +0700

    Setup the base table layout

--- admin/partials/contacts-list.php
+++ admin/partials/contacts-list.php
@@ -1,3 +1,40 @@
 <div class="wrap">
 	<h1><?php _e( 'Contacts', 'pipelab' ); ?></h1>
+
+	<table class="widefat">
+		<thead>
+		<tr>
+			<th class="row-title"><?php esc_attr_e( 'First Name', 'pipelab' ); ?></th>
+			<th><?php esc_attr_e( 'Last Name', 'pipelab' ); ?></th>
+			<th><?php esc_attr_e( 'Type', 'pipelab' ); ?></th>
+			<th><?php esc_attr_e( 'Owner', 'pipelab' ); ?></th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td class="row-title">
+				<label for="tablecell"><a href=""></a></label>
+			</td>
+			<td><a href=""></a></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="alternate">
+			<td class="row-title">
+				<label for="tablecell"><a href=""></a></label>
+			</td>
+			<td><a href=""></a></td>
+			<td></td>
+			<td></td>
+		</tr>
+		</tbody>
+		<tfoot>
+		<tr>
+			<th class="row-title"><?php esc_attr_e( 'First Name', 'pipelab' ); ?></th>
+			<th><?php esc_attr_e( 'Last Name', 'pipelab' ); ?></th>
+			<th><?php esc_attr_e( 'Type', 'pipelab' ); ?></th>
+			<th><?php esc_attr_e( 'Owner', 'pipelab' ); ?></th>
+		</tr>
+		</tfoot>
+	</table>
 </div>
