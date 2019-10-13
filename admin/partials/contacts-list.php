6fb28faf2f9c84bafa17b80f9e9affbe025d0f5a
Julien Liabeuf <julien@liabeuf.fr>
2019-10-13 18:11:22 +0700

    Update the contacts list page for use of WP_List_Table

--- admin/partials/contacts-list.php
+++ admin/partials/contacts-list.php
@@ -1,40 +1,18 @@
 <div class="wrap">
-	<h1><?php _e( 'Contacts', 'pipelab' ); ?></h1>
+	<h2><?php _e('Contacts', 'pipelab' ); ?></h2>
 
-	<table class="widefat">
-		<thead>
-		<tr>
-			<th class="row-title"><?php esc_attr_e( 'First Name', 'pipelab' ); ?></th>
-			<th><?php esc_attr_e( 'Last Name', 'pipelab' ); ?></th>
-			<th><?php esc_attr_e( 'Type', 'pipelab' ); ?></th>
-			<th><?php esc_attr_e( 'Owner', 'pipelab' ); ?></th>
-		</tr>
-		</thead>
-		<tbody>
-		<tr>
-			<td class="row-title">
-				<label for="tablecell"><a href=""></a></label>
-			</td>
-			<td><a href=""></a></td>
-			<td></td>
-			<td></td>
-		</tr>
-		<tr class="alternate">
-			<td class="row-title">
-				<label for="tablecell"><a href=""></a></label>
-			</td>
-			<td><a href=""></a></td>
-			<td></td>
-			<td></td>
-		</tr>
-		</tbody>
-		<tfoot>
-		<tr>
-			<th class="row-title"><?php esc_attr_e( 'First Name', 'pipelab' ); ?></th>
-			<th><?php esc_attr_e( 'Last Name', 'pipelab' ); ?></th>
-			<th><?php esc_attr_e( 'Type', 'pipelab' ); ?></th>
-			<th><?php esc_attr_e( 'Owner', 'pipelab' ); ?></th>
-		</tr>
-		</tfoot>
-	</table>
+	<div id="poststuff">
+		<div id="post-body" class="metabox-holder columns-2">
+			<div id="post-body-content">
+				<div class="meta-box-sortables ui-sortable">
+					<form method="post">
+						<?php
+						pipelab()->plugin_admin->contacts_list->prepare_items();
+						pipelab()->plugin_admin->contacts_list->display(); ?>
+					</form>
+				</div>
+			</div>
+		</div>
+		<br class="clear">
+	</div>
 </div>
